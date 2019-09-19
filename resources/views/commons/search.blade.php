91444721dbebaee57680b63d67266668b5a303cf
takuya.ishida <byhpw107@gmail.com>
2019-09-19 03:18:49 +0000

    change img

--- resources/views/commons/search.blade.php
+++ resources/views/commons/search.blade.php
@@ -2,6 +2,7 @@
     @include('commons.checkbox')
         
     <div class="form-group row justify-content-center">
+        
         <input name="keyword" type="text" class="form-control ml-3 col-sm-7">
         <button type="submit" class="btn btn-primary col-sm-1">検索</button>
     </div>
